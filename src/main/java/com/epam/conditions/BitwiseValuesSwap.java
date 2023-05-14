95eef52c319e0a192e29a4a7b382f3cb53ad0fa9
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-14 18:29:33 +0600

    kk

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,11 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first=(first^second);
+        second=second^(first);
+        first=second^(first);
+        System.out.println(first);
+        System.out.println(second);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,7 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        number = Integer.reverse(number);
+        System.out.println(number);
     }
-
 }
