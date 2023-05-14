a3096d48a0d8d4bd97e2ac31f7ca3bc6fa83e9c5
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-14 13:51:08 +0600

    1234

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -5,5 +5,21 @@ public class Animal {
     private int numberOfPaws;
     private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
 
+    public String getDescription(){
+        String hasFurStr ="no";
+        String moreThanOnePaw ="paws";
+        if (hasFur){
+            hasFurStr ="a";
+        }
+        if(this.numberOfPaws<2){
+            moreThanOnePaw = "paw";
+        }
+        return "This animal is mostly "+ this.color +"It has"+ numberOfPaws+" "+moreThanOnePaw+" and "+hasFurStr+" fur.";
+    }
 }
