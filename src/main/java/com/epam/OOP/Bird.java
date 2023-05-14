ce299a31bd96e8ea7b24132c208bea8c493e7dd4
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-14 13:54:39 +0600

    12345

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -4,10 +4,12 @@ public class Bird extends Animal{
 
     public Bird(){
         super("blue",2, false);
+        System.out.println("");
     }
 
     @Override
     public String getDescription() {
         return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
     }
+
 }
