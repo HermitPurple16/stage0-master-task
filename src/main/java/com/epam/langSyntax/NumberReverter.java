d1bb927d5a9f435c2e21a2cc5288dd57b53257e5
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-16 15:22:52 +0600

    xc

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,7 +3,19 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
-        number = Integer.reverse(number);
+        String strNum = ""+number;
+        char[] numCh = new char[3];
+        int j=numCh.length-1;
+        for (int i=0; i<numCh.length; i++){
+            numCh[i]=strNum.charAt(j);
+            j--;
+        }
+        number=0;
+        strNum="";
+        for(int i=0;i< numCh.length;i++){
+            strNum = (strNum +""+ numCh[i]);
+        }
+        number = Integer.parseInt(strNum);
         System.out.println(number);
     }
 }
