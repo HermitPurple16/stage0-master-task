c1863b5813313854d64feb70d42b553b7b680175
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-16 15:02:35 +0600

    mm

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if(firstSide+secondSide>thirdSide & thirdSide+secondSide>firstSide & firstSide+thirdSide>secondSide){
+        if(firstSide+secondSide>thirdSide && thirdSide+secondSide>firstSide && firstSide+thirdSide>secondSide){
             System.out.println("this is a valid triangle");
         } else{
             System.out.println("it's not a triangle");
