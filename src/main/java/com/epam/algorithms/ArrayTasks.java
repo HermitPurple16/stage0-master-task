bb0e6dee2388fc42c551093826ea26a7d09e68eb
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-16 14:57:12 +0600

    dd

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,8 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        String[] seasons = {"winter", "spring", "summer", "autumn"};
+        return seasons;
     }
 
     /**
@@ -23,7 +24,11 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] nums = new int[length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
+        }
+        return nums;
     }
 
     /**
@@ -34,7 +39,11 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -46,7 +55,15 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                res = (i + 1);
+            } else {
+                res = -1;
+            }
+        }
+        return res;
     }
 
     /**
@@ -58,7 +75,16 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        String[] demoArr = new String[arr.length];
+        int j = 0;
+        for (int i = arr.length - 1; i > -1; i--) {
+            demoArr[j] = arr[i];
+            j++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = demoArr[i];
+        }
+        return arr;
     }
 
     /**
@@ -70,7 +96,21 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        int length = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                length++;
+            }
+        }
+        int[] resArr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < resArr.length; j++) {
+                if (arr[i] > 0) {
+                    resArr[j] = arr[i];
+                }
+            }
+        }
+        return resArr;
     }
 
     /**
@@ -83,7 +123,47 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]] arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
-        return null;
+        int[] firstArr = new int[arr[0].length];
+        int[] secondArr = new int[arr[1].length];
+        for (int i = 0; i < arr[0].length; i++) {
+            firstArr[i] = arr[0][i];
+        }
+        for (int i = 0; i < arr[1].length; i++) {
+            secondArr[i] = arr[1][i];
+        }
+
+        for (int j = 0; j < 1000; j++) {
+            for (int i = 0; i < firstArr.length; i++) {
+                if ((i + 1) != firstArr.length) {
+                    if (firstArr[i] > firstArr[i + 1]) {
+                        int tmp = firstArr[i];
+                        firstArr[i] = firstArr[i + 1];
+                        firstArr[i + 1] = tmp;
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < 1000; j++) {
+            for (int i = 0; i < secondArr.length; i++) {
+                if ((i + 1) != secondArr.length) {
+                    if (secondArr[i] > secondArr[i + 1]) {
+                        int tmp = secondArr[i];
+                        secondArr[i] = secondArr[i + 1];
+                        secondArr[i + 1] = tmp;
+                    }
+                }
+            }
+        }
+
+        if(firstArr.length> secondArr.length){
+            int[][] newArr= new int[][]{secondArr, firstArr};
+            return newArr;
+        }else{
+            int[][] newArr= new int[][]{firstArr, secondArr};
+            return newArr;
+        }
+
     }
 
 }
